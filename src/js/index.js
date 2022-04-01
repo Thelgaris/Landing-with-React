1d09b88286590fcc21fd3a7d731e2f06cba00ef4
Thelgaris <alex.gabarron84@gmail.com>
2022-04-01 18:20:55 +0000

    onIt

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import { Component } from "react/cjs/react.production.min";
+
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+import Navbar from "./component/Navbar.jsx";
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
