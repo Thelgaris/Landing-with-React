30193a9ba203791dc8fe354d069f2826ded2b6e7
aemem <anamoliner.marques@gmail.com>
2022-04-01 20:37:37 +0000

    acabado

--- src/js/component/foot.jsx
+++ src/js/component/foot.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const foot = () => {
 	return (
-		<div
-			className="row text-center"
-			style={{ height: "300px", backgroundColor: "gray"}}>
+		<div className="row text-center" id="footer">
 			<h3>copyright ©website 2022 </h3>
 		</div>
 	);
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,7 @@ import Foot from "./foot.jsx";
 const Home = () => {
 	return (
 		<div className="container">
-			<div className="row">
+			<div className="row" id="cards">
 				<Card />
 				<Card />
 				<Card />
