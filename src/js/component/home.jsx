5e4066141248baa3ef151af724a44aa3c8129997
Thelgaris <alex.gabarron84@gmail.com>
2022-04-01 19:33:39 +0000

    2components

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Carrousel from "./Carrousel";
 //include images into your bundle
 
 //create your first component
 function Home() {
-	return <div>{Navbar}</div>;
+	return <div>{Navbar}</div>&
+	<div>{Carrousel}</div>;
 }
 
 export default Home;
